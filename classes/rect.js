c844c9f64a777438ce8c014b4eb5d08595a697eb
Dabab001 <Dabab001@gold.ac.uk>
2024-12-12 12:54:42 +0000

    finally centered and mouse collision

--- classes/rect.js
+++ classes/rect.js
@@ -1,12 +1,11 @@
 export default class Rect {
-    constructor(p5,position,color=[9,7,140],size = [50,50],border = 1){
+    constructor(p5,position,size = [50,50],color=[9,7,140],border = 1){
       this.position= p5.createVector(...position)
-      this.x = this.top = this.position.y
-      this.y =this.left = this.position.x;
+      this.top = this.position.y
+      this.left = this.position.x;
       [this.width, this.height] = size
       this.bottom = this.top + this.height;
       this.right = this.left + this.width;
-      this.center = p5.createVector(...[this.left+ (this.width/2),this.top+ (this.height/2)])
       this.strokeWeight = border
       this.color = color
 
@@ -19,15 +18,24 @@ export default class Rect {
       }
       return false;
     }
+    get_center(p5){
+      return p5.createVector(...[this.left+ (this.width/2),this.top+ (this.height/2)])
+    }
+    update(new_position){
+      this.position.set(...new_position)
+      this.top = this.position.y
+      this.left = this.position.x;
+      this.bottom = this.top + this.height;
+      this.right = this.left + this.width;
+    }
 
-    
-  
    
   
-    draw(p5, scroll_offset = [0, 0]) {
+    draw(p5) {
       p5.fill(this.color);
       p5.strokeWeight(this.strokeWeight);
+      
 
-      p5.rect(this.position.x + scroll_offset[0],this.position.y + scroll_offset[1],this.width,this.height); 
+      p5.rect(this.position.x,this.position.y,this.width,this.height); 
     }
   }
--- sketch.js
+++ sketch.js
@@ -1,13 +1,18 @@
 import Rect from "./classes/rect.js";
-let squares;
+let squares,mouserect,big;
+
+
+
 function createGrid(p5, startPos, distance) {
   let squareArray = [];
-  distance+= 50
+  distance+= 30
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       let x = startPos[0] + distance * i; 
       let y = startPos[1] + distance * j; 
-      squareArray.push(new Rect(p5, [x, y]));
+      squareArray.push(new Rect(p5, [x, y],[30,30]));
+      
+  
     }
   }
   
@@ -20,8 +25,8 @@ new p5(function(p5){
 
 p5.setup = function() {
   p5.createCanvas(400, 400);
-  squares= createGrid(p5,[115,115],10)
-
+  squares= createGrid(p5,[125,125],30)
+  mouserect = new Rect(p5,[p5.mouseX,p5.mouseY],[12,18],[255,0,0],0)
   let firstposition = squares[0].position.array()
   let lastposition = [squares[squares.length-1].right,squares[squares.length-1].bottom]
   console.log(firstposition,lastposition)
@@ -29,9 +34,20 @@ p5.setup = function() {
 
  p5.draw = function() {
   p5.background("#0000B0");
+  mouserect.update([p5.mouseX,p5.mouseY])
+  
   squares.forEach(square => {
+    if (mouserect.colliderect(square)){
+      square.color = "red"
+    } else{
+      square.color = [9,7,140]
+    }
     square.draw(p5)
   });
 
+  // if (big.collidepoint(mouserect)){
+
+  // }
+  mouserect.draw(p5)
 
 }})
