ab71df5f801ee6135c36261476d2b048fd36006a
dabab001 <Dabab001@gold.ac.uk>
2024-12-17 23:28:21 +0000

    MIght have to make a selected card variable

--- classes/Game.js
+++ classes/Game.js
@@ -3,6 +3,7 @@ import Rect from "./Rect.js";
 export default class Game{
     constructor (p5){
         this.card = new Card(p5,[0,0]);
+        this.card2 = new Card(p5,[100,0]);
         this.gridrects = [];
         this.lines = []
     }
@@ -19,10 +20,18 @@ export default class Game{
     }
     setup(p5){
         this.createGrid(p5,[105,105],30)
+        let resetButton = p5.createButton('Reset Page');
+        resetButton.position(450, 10);
+        
+        // Assign a function to the button's mousePressed event
+        resetButton.mousePressed(this.resetPage);
         
         // let firstposition = this.gridrects[0].position.array()
         // let lastposition = [this.gridrects[this.gridrects.length-1].right,this.gridrects[this.gridrects.length-1].bottom]
         // console.log(firstposition,lastposition)
+    }
+    resetPage() {
+      window.location.reload();
     }
     
     render(p5){
@@ -32,10 +41,16 @@ export default class Game{
           this.card.lock_to_center(...position)
           this.card.clicked= false
         }
+        if (square.colliderect(this.card2) && !p5.mouseIsPressed){
+          let position = square.get_center(p5).array()
+          this.card2.lock_to_center(...position)
+          this.card2.clicked= false
+        }
         square.draw(p5)
       }); 
       
       this.card.draw(p5)
+      this.card2.draw(p5)
       
     }
 }
